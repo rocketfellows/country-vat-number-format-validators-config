013de66bff691f122bb5a4efc714821c8be884ec
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-04-19 20:39:46 +0400

    [CVFV-27] added country mock.

--- tests/unit/CountryVatNumberValidatorsConfigInterfaceTest.php
+++ tests/unit/CountryVatNumberValidatorsConfigInterfaceTest.php
@@ -2,6 +2,8 @@
 
 namespace rocketfellows\CountryVatNumberFormatValidatorsConfig\tests\unit;
 
+use arslanimamutdinov\ISOStandard3166\Country;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
 abstract class CountryVatNumberValidatorsConfigInterfaceTest extends TestCase
@@ -17,5 +19,32 @@ abstract class CountryVatNumberValidatorsConfigInterfaceTest extends TestCase
         foreach (self::EXPECTED_IMPLEMENTED_INTERFACES as $expectedImplementedInterface) {
             $this->assertInstanceOf($expectedImplementedInterface, $this->config);
         }
+    }
+
+    /**
+     * @return Country|MockObject
+     */
+    protected function getCountryMock(array $params = []): Country
+    {
+        /** @var Country|MockObject $mock */
+        $mock = $this->createMock(Country::class);
+
+        if (isset($params['alpha2'])) {
+            $mock->method('getAlpha2')->willReturn($params['alpha2']);
+        }
+
+        if (isset($params['alpha3'])) {
+            $mock->method('getAlpha3')->willReturn($params['alpha3']);
+        }
+
+        if (isset($params['numericCode'])) {
+            $mock->method('getNumericCode')->willReturn($params['numericCode']);
+        }
+
+        if (isset($params['name'])) {
+            $mock->method('getName')->willReturn($params['name']);
+        }
+
+        return $mock;
     }
 }
